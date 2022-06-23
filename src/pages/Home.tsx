799f4c45698be1e940bfe9cb346a61cdd2f23154
Jithinqw <jithin.mscis3@iiitmk.ac.in>
2022-06-23 15:55:36 +0530

    Adding default text

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,32 @@
+import {
+    Heading
+} from 'wafflecss';
+import './Home.css';
+
 const Home = () => {
     return (
-        <div>Home page</div>
+        <div>
+            <div className="wrapper">
+                <Heading 
+                    data={{
+                        headingText: 'We will be back with Docs.'
+                    }}
+                    options={{
+                        size: 'extralarge'
+                    }}
+                />
+            </div>
+            <div className='wrapper'>
+                <Heading 
+                    data={{
+                        headingText: 'These pages are build with WaffleCSS, eventhough it is still in Beta.'
+                    }}
+                    options={{
+                        size: 'large'
+                    }}
+                />
+            </div>
+        </div>
     )
 }
 
--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,6 +1,25 @@
+import { Heading } from 'wafflecss';
+
 const NotFound = () => {
     return (
-        <div>Notfound page</div>
+        <div>
+            <Heading 
+                data={{
+                    headingText: "Looks like you hit a road block."
+                }}
+                options={{
+                    size: 'extralarge'
+                }}
+            />
+            <Heading 
+                data={{
+                    headingText: "Sorry for not adding any fancy animations here."
+                }}
+                options={{
+                    size: 'large'
+                }}
+            />
+        </div>
     )
 }
 
