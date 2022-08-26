aa0a65794bae8aac2f134af3f36f5b9d86cbf4b3
Jithinqw <jithin.mscis3@iiitmk.ac.in>
2022-08-26 15:37:53 +0530

    Update Pages

---
+++ src/api/cms.ts
@@ -0,0 +1,28 @@
+const waffleCMS = {
+    buttonCMS: [
+        {githubCMS: 'Github', name: 'github'},
+        {getStartedCMS: 'Get Started', name: 'Get Started'},
+    ],
+    mainHeading: 'Build your next website even faster with premade responsive components designed and built by Mantine maintainers and community. All components are free forever for everyone.',
+    features: [
+        {
+            heading: 'Fast & Light-Weight',
+            headingExplain: 'WaffleCSS is having lowest footprint for your application, making it one of the fastest.'
+        },
+        {
+            heading: 'Composable',
+            headingExplain: 'Change colors, fonts, shadows and other properties, or even send you own custom CSS into component.'
+        },
+        {
+            heading: 'Free for everyone',
+            headingExplain: 'Free, open source, community-driven, MIT license. Use anywhere, including commercial projects.'
+        }
+    ],
+    components: [
+        {componentType: 'Promotions', number: 6},
+        {componentType: 'Basic', number: 7},
+        {componentType: 'Legal', number: 1},
+    ],
+}
+
+export default waffleCMS;
--- src/common/Hero/Hero.tsx
+++ src/common/Hero/Hero.tsx
@@ -1,6 +1,10 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFire, faCompass, faHeart, faHand } from '@fortawesome/free-solid-svg-icons';
 import { Heading, Button } from 'wafflecss';
+import waffleCMS from '../../api/cms';
 import { Utilities } from '../../api/utilities';
 import './Hero.css';
+import { SizeProp } from '@fortawesome/fontawesome-svg-core';
 
 const Hero = () => {
 
@@ -16,9 +20,49 @@ const Hero = () => {
             case 'mobile':
                 return 'sm';
             case 'tablet':
-                return 'lg';
+                return 'md';
             default:
                 return 'md';
+        }
+    }
+
+    const resolveIconSize = (): SizeProp => {
+        const screenSize = Utilities.resolveScreenSize();
+        switch(screenSize) {
+            case 'desktop':
+                return "3x";
+            case 'mobile':
+                return "1x";
+            case 'tablet':
+                return "2x";
+            default:
+                return "2x";
+        }
+    }
+
+    const redirectToGithub = () => {
+        window.open('https://github.com/Jithinqw/wafflecss');
+    }
+
+    const redirectToDoc = () => {
+        window.open('');
+    }
+
+    /**
+     * @function resolveIcon
+     * @param {number} i 
+     * @returns {IconDefinition}
+     */
+    const resolveIcon = (i: number) => {
+        switch(i) {
+            case 0:
+                return faFire;
+            case 1:
+                return faCompass;
+            case 2:
+                return faHeart;
+            default:
+                return faHand;
         }
     }
 
@@ -40,27 +84,72 @@ const Hero = () => {
                 <div className='buttonWCover'>
                     <Button 
                         data={{
-                            displayText: 'Get Started'
+                            displayText: Object.values(waffleCMS.buttonCMS)[1].getStartedCMS
                         }}
                         options={{
                             size: resolveButtonSize(),
                             variants: 'primary'
                         }}
                         events={{
+                            onClick: redirectToDoc
                         }}
                     />
                 </div>
                 <div className='buttonWCover'>
                     <Button 
                         data={{
-                            displayText: 'Github'
+                            displayText: Object.values(waffleCMS.buttonCMS)[0].githubCMS
                         }}
                         options={{
                             size: resolveButtonSize(),
                             variants: 'success'
                         }}
+                        events={{
+                            onClick: redirectToGithub
+                        }}
                     />
                 </div>
+            </div>
+            <div className='heroFeaturesContainer'>
+                {
+                    waffleCMS.features && 
+                    waffleCMS.features.length > 0 &&
+                    Object.keys(waffleCMS.features).map((e, i) => {
+                        return (
+                            <div key={i}>
+                                <div className='heroIconContainer'>
+                                    <FontAwesomeIcon 
+                                        icon={resolveIcon(i)}
+                                        color={'grey'}
+                                        size={resolveIconSize()}
+                                    />
+                                </div>
+                                <div className='heroIconJustify'>
+                                    <Heading
+                                        data={{
+                                            headingText: Object.values(waffleCMS.features)[i].heading
+                                        }}
+                                        options={{
+                                            size: 'normal',
+                                            headingColor: 'black',
+                                            className: 'heroIconHeading'
+                                        }}
+                                    />
+                                    <Heading
+                                        data={{
+                                            headingText: Object.values(waffleCMS.features)[i].headingExplain
+                                        }}
+                                        options={{
+                                            size: 'small',
+                                            headingColor: 'black',
+                                            className: 'heroIconExplain'
+                                        }}
+                                    />
+                                </div>
+                            </div>
+                        )
+                    })
+                }
             </div>
         </>
     )
